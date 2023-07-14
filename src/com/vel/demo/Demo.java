350462246a3ead706c3f108ca0b8317f119d1bf7
VikasKale <vikaask2022@gmail.com>
2023-07-14 23:10:15 +0530

    Added new code

---
+++ src/com/vel/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.vel.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to github session phase 1");
+		System.out.println("welcome again");
+
+	}
+
+}
