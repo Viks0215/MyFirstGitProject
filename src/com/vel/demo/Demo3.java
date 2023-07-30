2ef6326f1917bd76cc633094b320c55960ea0fd8
Vishal Deshmukh <vishaldeshmukh963@gmail.com>
2023-07-30 17:22:19 +0530

    one code added by vishal

--- src/com/vel/demo/Demo3.java
+++ src/com/vel/demo/Demo3.java
@@ -4,6 +4,12 @@ public class Demo3 {
 	
 	public static void main(String[] args) {
 		System.out.println("github session 3 local branch code");
+		
+		for (int i = 0; i <100; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
