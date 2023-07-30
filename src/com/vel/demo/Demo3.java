65379d368bc69d7ee71eb3a2b725d09de3888cbf
Vishal Deshmukh <vishaldeshmukh963@gmail.com>
2023-07-30 17:51:45 +0530

    add 2 code for Even & odd

--- src/com/vel/demo/Demo3.java
+++ src/com/vel/demo/Demo3.java
@@ -2,14 +2,32 @@ package com.vel.demo;
 
 public class Demo3 {
 	
-	public static void main(String[] args) {
-		System.out.println("github session 3 local branch code");
-		
-		for (int i = 0; i <100; i++) {
+	void evenNumber() {
+		System.out.println("The Even Number Of 1 to 200 ");
+		for (int i = 1; i <200; i++) {
 			if (i%2==0) {
 				System.out.println(i);
 			}
-		}
+	    }
+	}
+	
+	void oddNumber() {
+		System.out.println("The odd Number Of 1 to 200 ");
+		for (int i = 1; i <200; i++) {
+			if (i%2!=0) {
+				System.out.println(i);
+			}
+	    }
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Demo3 d3 = new Demo3();
+		d3.evenNumber();
+		d3.oddNumber();
+			
+		
 	}
 
 }
